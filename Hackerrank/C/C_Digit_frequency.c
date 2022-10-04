69e26244d20ffdc4c7f5a563eefa0d6105d2238e
Hari om <hariom24568@gmail.com>
2022-10-04 23:41:39 +0530

    C_Digit_Frequency

---
+++ Hackerrank/C/C_Digit_frequency.c
@@ -0,0 +1,28 @@
+/*
+https://www.hackerrank.com/challenges/frequency-of-digits-1/problem
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main() {
+    char *s;
+    s = malloc(1024 * sizeof(char));
+    scanf("%s", s);
+    s = realloc(s, strlen(s) + 1);
+    int len = strlen(s), i;
+    int arr[10];
+    for(i = 0; i < 10; i++)
+        arr[i] = 0;
+    for(i = 0; i < len; i++) {
+        if(s[i] >= '0' && s[i] <= '9') {
+            arr[(int)(s[i] - '0')]++;
+        }
+    }
+    for(i = 0; i < 10; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+    
+    return 0;
+}
