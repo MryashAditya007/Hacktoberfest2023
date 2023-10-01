deeb83610416942ec1f75772b8b12f3088a78368
Yashwant Aditya <114852264+MryashAditya007@users.noreply.github.com>
2023-10-01 14:42:14 +0530

    Create Logic Error.java

---
+++ Problems/public class DebugExample {     public static void main(String[] args) {         int x = 10;         int y = 5;         int sum = x - y;  /Logic Error.java
@@ -0,0 +1,8 @@
+public class DebugExample {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+        int sum = x - y;  // This should be x + y.
+        System.out.println("Sum: " + sum);
+    }
+}
