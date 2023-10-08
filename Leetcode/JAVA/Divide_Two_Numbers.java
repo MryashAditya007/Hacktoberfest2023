adda9e2a8983caf90a45bae2f2f50893ea827e1c
Yashwant Aditya <114852264+MryashAditya007@users.noreply.github.com>
2023-10-08 14:51:02 +0530

    Delete Leetcode/JAVA directory

--- Leetcode/JAVA/Divide_Two_Numbers.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int divide(int dividend, int divisor) {
-        if(dividend==-2147483648&&divisor==-1)
-        return 2147483647;
-        double ans=dividend/divisor;
-        int a=(int)Math.round(ans);
-        return a;
-    }
-}
