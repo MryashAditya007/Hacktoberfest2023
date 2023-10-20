360bd1a0a228860d9342878a6f9141c4b3ba46d2
adinathraut <82745544+adinathraut@users.noreply.github.com>
2023-10-20 16:02:58 +0530

    Create QuickSort.cpp

---
+++ C++/QuickSort.cpp
@@ -0,0 +1,20 @@
+// C++ Implementation of the Quick Sort Algorithm.
+#include <iostream>
+using namespace std;
+
+int partition(int arr[], int start, int end)
+{
+
+	int pivot = arr[start];
+
+	int count = 0;
+	for (int i = start + 1; i <= end; i++) {
+		if (arr[i] <= pivot)
+			count++;
+	}
+
+	// Giving pivot element its correct position
+	int pivotIndex = start + count;
+	swap(arr[pivotIndex], arr[start]);
+
+	
